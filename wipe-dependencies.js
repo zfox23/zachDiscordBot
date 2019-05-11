80199433f65cbf8553c30cd50dfdd8d1ad39a137
Zach Fox <valefox23@gmail.com>
2019-05-11 19:18:33 +0000

    Add wipe-dependencies

---
+++ wipe-dependencies.js
@@ -0,0 +1,19 @@
+// From https://medium.com/@jh3y/how-to-update-all-npm-packages-in-your-project-at-once-17a8981860ea
+const fs = require('fs')
+const wipeDependencies = () => {
+  const file  = fs.readFileSync('package.json')
+  const content = JSON.parse(file)
+  for (const devDep in content.devDependencies) {
+    content.devDependencies[devDep] = '*'
+  }
+  for (const dep in content.dependencies) {
+    content.dependencies[dep] = '*'
+  }
+  fs.writeFileSync('package.json', JSON.stringify(content))
+}
+
+if (require.main === module) {
+  wipeDependencies()
+} else {
+  module.exports = wipeDependencies
+}
